f83627e251704be5de275b54b3d6ab0e9ac47236
vivihm <viviana.marca@sptech.school>
2022-11-30 00:00:12 -0300

    Alterações no css da página comentários

--- site/public/js/funcoes.js
+++ site/public/js/funcoes.js
@@ -4,7 +4,6 @@ function validarSessao() {
 
     var email = sessionStorage.EMAIL_USUARIO;
     var nome = sessionStorage.NOME_USUARIO;
-    var verificar = sessionStorage.RESPONDEU
 
     var b_usuario = document.getElementById("b_usuario");
 
